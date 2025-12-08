9f75b278bfe5682d8c1513c8482c76d4afcdeca8
saadfahad1992MD <210144189+saadfahad1992MD@users.noreply.github.com>
2025-12-08 17:41:36 -0500

    Add comprehensive medication database with 15 options and real product images
    - Add Tylenol brand (Suspensión Infantil + Pediátrica)
    - Add Tempra Fen (ibuprofen)
    - Add generic brands (Similares, del Ahorro, Pharmalife, Benavides)
    - Add 4 suppository options (Tempra + Generic 100mg/300mg)
    - Include real product images for all medications
    - Total: 6 paracetamol + 5 ibuprofen + 4 suppositories = 15 options

--- client/src/data/medicationsMexico.js
+++ client/src/data/medicationsMexico.js
@@ -3,6 +3,7 @@
 
 export const medicationsMexico = {
   paracetamol: [
+    // TEMPRA BRAND
     {
       id: 'tempra-jarabe',
       name: 'Tempra Jarabe Infantil',
@@ -10,8 +11,8 @@ export const medicationsMexico = {
       concentration: 160, // mg per 5ml
       volume: 5, // ml
       form: 'Jarabe',
-      image: '/medications/tempra.jpg',
-      ageRestriction: ''
+      image: '/medications/tempra-jarabe.jpg',
+      ageRestriction: '2-11 años'
     },
     {
       id: 'tempra-gotas',
@@ -20,30 +21,109 @@ export const medicationsMexico = {
       concentration: 100, // mg per ml
       volume: 1, // ml
       form: 'Gotas',
-      image: '/medications/tempra.jpg',
+      image: '/medications/tempra-gotas.jpg',
       ageRestriction: '0-36 meses'
+    },
+    // TYLENOL BRAND
+    {
+      id: 'tylenol-infantil',
+      name: 'Tylenol Suspensión Infantil',
+      ingredient: 'Paracetamol',
+      concentration: 160, // mg per 5ml
+      volume: 5, // ml
+      form: 'Jarabe',
+      image: '/medications/tylenol-infantil.jpg',
+      ageRestriction: '2-12 años'
+    },
+    {
+      id: 'tylenol-pediatrico',
+      name: 'Tylenol Suspensión Pediátrica',
+      ingredient: 'Paracetamol',
+      concentration: 100, // mg per ml
+      volume: 1, // ml
+      form: 'Gotas',
+      image: '/medications/tylenol-pediatrico.jpg',
+      ageRestriction: '0-24 meses'
+    },
+    // GENERIC BRANDS
+    {
+      id: 'paracetamol-jarabe-generico',
+      name: 'Paracetamol Jarabe Infantil (Genérico)',
+      ingredient: 'Paracetamol',
+      concentration: 160, // mg per 5ml
+      volume: 5, // ml
+      form: 'Jarabe',
+      image: '/medications/tempra-jarabe.jpg',
+      ageRestriction: '2-11 años',
+      note: 'Similares, del Ahorro, Pharmalife, Benavides'
+    },
+    {
+      id: 'paracetamol-gotas-generico',
+      name: 'Paracetamol Solución Pediátrica (Genérico)',
+      ingredient: 'Paracetamol',
+      concentration: 100, // mg per ml
+      volume: 1, // ml
+      form: 'Gotas',
+      image: '/medications/tempra-gotas.jpg',
+      ageRestriction: '0-36 meses',
+      note: 'Similares, del Ahorro, Pharmalife, Benavides'
     },
   ],
   ibuprofen: [
+    // MOTRIN BRAND
     {
       id: 'motrin-infantil',
-      name: 'Motrin Infantil',
+      name: 'Motrin Suspensión Infantil',
       ingredient: 'Ibuprofeno',
       concentration: 100, // mg per 5ml
       volume: 5, // ml
       form: 'Suspensión',
-      image: '/medications/motrin.jpg',
-      ageRestriction: 'Edad mayor a 6 meses'
+      image: '/medications/motrin-infantil.jpg',
+      ageRestriction: '2-12 años'
     },
     {
       id: 'motrin-pediatrico',
-      name: 'Motrin Pediátrico',
+      name: 'Motrin Suspensión Pediátrica',
       ingredient: 'Ibuprofeno',
       concentration: 40, // mg per ml
       volume: 1, // ml
       form: 'Gotas',
-      image: '/medications/motrin.jpg',
-      ageRestriction: 'Edad mayor a 6 meses'
+      image: '/medications/motrin-pediatrico.jpg',
+      ageRestriction: '6-24 meses'
+    },
+    // TEMPRA FEN BRAND
+    {
+      id: 'temprafen-infantil',
+      name: 'Tempra Fen Suspensión Infantil',
+      ingredient: 'Ibuprofeno',
+      concentration: 100, // mg per 5ml
+      volume: 5, // ml
+      form: 'Suspensión',
+      image: '/medications/motrin-infantil.jpg',
+      ageRestriction: '2-12 años'
+    },
+    // GENERIC BRANDS
+    {
+      id: 'ibuprofeno-suspension-generico',
+      name: 'Ibuprofeno Suspensión Infantil (Genérico)',
+      ingredient: 'Ibuprofeno',
+      concentration: 100, // mg per 5ml
+      volume: 5, // ml
+      form: 'Suspensión',
+      image: '/medications/motrin-infantil.jpg',
+      ageRestriction: '2-12 años',
+      note: 'Similares, del Ahorro'
+    },
+    {
+      id: 'ibuprofeno-gotas-generico',
+      name: 'Ibuprofeno Suspensión Pediátrica (Genérico)',
+      ingredient: 'Ibuprofeno',
+      concentration: 40, // mg per ml
+      volume: 1, // ml
+      form: 'Gotas',
+      image: '/medications/motrin-pediatrico.jpg',
+      ageRestriction: '6-24 meses',
+      note: 'Similares, del Ahorro'
     },
   ],
   suppositories: {
@@ -69,6 +149,30 @@ export const medicationsMexico = {
         weightRange: '12-30 kg',
         ageRange: '18 meses-12 años',
         ageRestriction: 'Para niños mayores'
+      },
+      {
+        id: 'paracetamol-supp-100-generico',
+        name: 'Paracetamol Supositorio 100mg (Genérico)',
+        ingredient: 'Paracetamol',
+        concentration: 100, // mg per suppository
+        form: 'Supositorio',
+        image: '/medications/tempra.jpg',
+        weightRange: '3-12 kg',
+        ageRange: '1-18 meses',
+        ageRestriction: 'Para bebés y niños pequeños',
+        note: 'Piremol, Marca del Ahorro, Similares'
+      },
+      {
+        id: 'paracetamol-supp-300-generico',
+        name: 'Paracetamol Supositorio 300mg (Genérico)',
+        ingredient: 'Paracetamol',
+        concentration: 300, // mg per suppository
+        form: 'Supositorio',
+        image: '/medications/tempra.jpg',
+        weightRange: '12-30 kg',
+        ageRange: '18 meses-12 años',
+        ageRestriction: 'Para niños mayores',
+        note: 'Piremol, Marca del Ahorro, Similares'
       }
     ],
     diclofenac: []
